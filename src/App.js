449d7fed78d08a67aa3f76a0a53ee0c1a556395b
Igor Marcos Riegel <igor.riegel@gmail.com>
2021-03-29 17:26:46 -0300

    Adicionado stilo e layout basico a pagina de Login

--- src/App.js
+++ src/App.js
@@ -1,7 +1,18 @@
-import React from 'react';
+import React, { Component } from 'react';
+import Login from './pages/Login';
+import Wallet from './pages/Wallet'
+import './App.css'
 
-function App() {
-  return <div>My TrybeWallet $$$$!</div>;
-}
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Login />
+        <Wallet />
+      </div>
+    );
+  }
+};
 
 export default App;
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,8 +1,25 @@
 import React from 'react';
+import './Login.css'
 
 class Login extends React.Component {
   render() {
-    return <div>Login</div>;
+    return (
+      <>
+        <h1>Trybe Wallet</h1>
+        <form action="">
+          <label for="user">
+            user:
+            <input type="text" name="user" id="" />
+          </label>
+          <label for="password">
+            password:
+            <input type="text" name="password" id="" />
+          </label>
+          <button type="submit">Login</button>
+        </form>
+      </>
+
+    );
   }
 }
 
