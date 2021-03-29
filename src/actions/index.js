702598b7aa02d12398b116c94eb325d340100b39
Henrique Brito Elias <henriquebelias@gmail.com>
2021-03-29 17:23:41 -0300

    Requisito 1 - Pagina de Login

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,1 +1,4 @@
 // Coloque aqui suas actions
+import userLogin from './userAction';
+
+export default userLogin;
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,9 +1,110 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+import userLogin from '../actions';
+
+const PASSWORD_MINIMUM_LENGTH = 6;
 
 class Login extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      emailInput: '',
+      passwordInput: '',
+      formErrors: {
+        emailInput: '',
+        passwordInput: '',
+      },
+      typed: false,
+    };
+
+    this.handleChange = this.handleChange.bind(this);
+    this.validateField = this.validateField.bind(this);
+  }
+
+  handleChange(event) {
+    const { name, value } = event.target;
+    this.setState((state) => ({
+      ...state,
+      [name]: value,
+      formErrors: {
+        ...state.formErrors,
+        [name]: this.validateField(name, value),
+      },
+      typed: true,
+    }));
+  }
+
+  /* Validation found at: https://github.com/tryber/sd-09-live-lectures/tree/lecture/12.2 */
+  validateField(fieldName, value) {
+    const isValid = value.match(/^([\w.%+-]+)@([\w-]+\.)+([\w]{2,})$/i);
+    switch (fieldName) {
+    case 'emailInput':
+      return isValid ? '' : 'Email is invalid';
+    case 'passwordInput':
+      return value.length >= PASSWORD_MINIMUM_LENGTH ? '' : 'Password is too short';
+    default:
+      break;
+    }
+    return '';
+  }
+
   render() {
-    return <div>Login</div>;
+    const {
+      emailInput,
+      passwordInput,
+      formErrors: { emailInput: emailError, passwordInput: passwordError },
+      typed,
+    } = this.state;
+    const { logIn } = this.props;
+    return (
+      <div>
+        <label htmlFor="email-input">
+          Email:
+          <input
+            data-testid="email-input"
+            type="text"
+            id="email-input"
+            name="emailInput"
+            value={ emailInput }
+            onChange={ this.handleChange }
+            autoComplete="off"
+          />
+        </label>
+        <label htmlFor="password-input">
+          Senha:
+          <input
+            data-testid="password-input"
+            type="password"
+            id="password-input"
+            name="passwordInput"
+            value={ passwordInput }
+            onChange={ this.handleChange }
+            autoComplete="off"
+          />
+        </label>
+        <Link to="/carteira">
+          <button
+            type="button"
+            disabled={ !typed || emailError || passwordError }
+            onClick={ () => logIn(emailInput) }
+          >
+            Entrar
+          </button>
+        </Link>
+      </div>
+    );
   }
 }
 
-export default Login;
+const mapDispatchToProps = (dispatch) => ({
+  logIn: (email) => dispatch(userLogin(email)),
+});
+
+Login.propTypes = {
+  logIn: PropTypes.func.isRequired,
+};
+
+export default connect(null, mapDispatchToProps)(Login);
