dddf3ec3eb7ac1806caa304fcd81098fbb009e47
BBrolesi <brunohsbrolesi@gmail.com>
2021-03-29 15:52:53 -0300

    Add Login Page, and fields rules validation

--- src/App.js
+++ src/App.js
@@ -1,7 +1,15 @@
 import React from 'react';
+import { Route, Switch } from 'react-router';
+import Login from './pages/Login';
 
 function App() {
-  return <div>Hello, TrybeWallet!</div>;
+  return (
+    <div className="App">
+      <Switch>
+        <Route exact path="/"><Login /></Route>
+      </Switch>
+    </div>
+  );
 }
 
 export default App;
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,8 +1,66 @@
+import { Button, TextField } from '@material-ui/core';
 import React from 'react';
 
 class Login extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      email: '',
+      password: '',
+    };
+
+    this.handleInputChange = this.handleInputChange.bind(this);
+    this.validateLoginFields = this.validateLoginFields.bind(this);
+  }
+
+  validateLoginFields(email, password) {
+    const re = /[^@]+@[^.]+\..+/g;
+    const minimumPasswordLength = 6;
+    const emailTest = re.test(String(email).toLowerCase());
+    const passwordTest = password.length >= minimumPasswordLength;
+    console.log(emailTest);
+    console.log(passwordTest);
+    return emailTest && passwordTest;
+  }
+
+  handleInputChange({ target }) {
+    const value = target.type === 'checkbox' ? target.checked : target.value;
+    const { name } = target;
+
+    this.setState({
+      [name]: value,
+    });
+  }
+
   render() {
-    return <div>Login</div>;
+    const { email, password } = this.state;
+    const joinButton = this.validateLoginFields(email, password)
+      ? <Button>Entrar</Button> : <Button disabled>Entrar</Button>;
+    return (
+      <div>
+        <TextField
+          id="input-email"
+          name="email"
+          label="Login"
+          data-testid="email-input"
+          variant="outlined"
+          type="email"
+          value={ email }
+          onChange={ this.handleInputChange }
+        />
+        <TextField
+          id="input-password"
+          name="password"
+          label="Senha"
+          data-testid="password-input"
+          variant="outlined"
+          type="password"
+          value={ password }
+          onChange={ this.handleInputChange }
+        />
+        {joinButton}
+      </div>
+    );
   }
 }
 
