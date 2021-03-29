4347ef57986337c0ee83ef495bef9e5287253f74
Khyrki <khyrkimitsunobi@gmail.com>
2021-03-29 17:58:19 -0300

    Add requisito 1

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,1 +1,7 @@
 // Coloque aqui suas actions
+const userEmailDispach = (email) => ({
+  type: 'USER_EMAIL_DISPACH',
+  userEmail: email,
+});
+
+export default userEmailDispach;
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,9 +1,92 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import { connect } from 'react-redux';
+import userEmailDispach from '../actions';
 
 class Login extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      email: '',
+      password: '',
+      validatedEmail: false,
+      validatedPassword: false,
+      disableBtn: true,
+    };
+
+    this.handleChange = this.handleChange.bind(this);
+    this.credentialValidation = this.credentialValidation.bind(this);
+    this.sendBtnClick = this.sendBtnClick.bind(this);
+  }
+
+  credentialValidation(target) {
+    const { validatedEmail, validatedPassword } = this.state;
+    const email = new RegExp(/^[\w.]+@[a-z]+.\w{2,3}$/g);
+    const password = new RegExp(/[\w\D]{5}/g);
+    if (target.name === 'email') {
+      this.setState({ validatedEmail: email.test(target.value) });
+    }
+    if (target.name === 'password') {
+      this.setState({ validatedPassword: password.test(target.value) });
+    }
+    if (validatedEmail === true && validatedPassword === true) {
+      this.setState({ disableBtn: false });
+    }
+  }
+
+  handleChange({ target }) {
+    this.setState({ [target.name]: target.value });
+    this.setState({
+      disableBtn: true,
+    });
+    this.credentialValidation(target);
+  }
+
+  sendBtnClick() {
+    const { userEmailDispach } = this.props;
+    const { email } = this.state;
+    userEmailDispach(email);
+  }
+
   render() {
-    return <div>Login</div>;
+    const { email, password, disableBtn } = this.state;
+    return (
+      <div>
+        <label htmlFor="email">
+          Login:
+          <input
+            name="email"
+            value={ email }
+            data-testid="email-input"
+            onChange={ this.handleChange }
+          />
+        </label>
+        <label htmlFor="password">
+          Senha:
+          <input
+            type="password"
+            name="password"
+            value={ password }
+            data-testid="password-input"
+            onChange={ this.handleChange }
+          />
+        </label>
+        <Link to="/carteira">
+          <button
+            type="button"
+            disabled={ disableBtn }
+            onClick={ this.sendBtnClick }
+          >
+            Entrar
+          </button>
+        </Link>
+      </div>
+    );
   }
 }
 
-export default Login;
+const mapDispatchToProps = (dispach) => ({
+  userEmailDispach: (email) => dispach(userEmailDispach(email)),
+});
+
+export default connect(null, mapDispatchToProps)(Login);
