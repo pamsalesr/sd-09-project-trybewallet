461e8108a6eb535fa63de45a30c46f6d6afaf7c6
jiarguello <jhonatan.arguello@gmail.com>
2021-03-29 16:03:31 -0300

    Partial implementation os requirement 1.

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -2,7 +2,21 @@ import React from 'react';
 
 class Login extends React.Component {
   render() {
-    return <div>Login</div>;
+    return (
+      <div>
+        <h1>Jhon Wallet</h1>
+        <form>
+          <label htmlFor="email">
+            E-mail
+            <input type="email" name="email" data-testid="email-input" />
+          </label>
+          <label htmlFor="senha">
+            Senha
+            <input type="password" name="senha" data-testid="password-input" />
+          </label>
+        </form>
+      </div>
+    );
   }
 }
 
