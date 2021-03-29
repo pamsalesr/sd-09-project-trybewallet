ec3b9790dc69a69a173153f8293fab359b4331c7
MarcioFujita <hanker78@gmail.com>
2021-03-29 18:27:28 -0300

    Requisito 1 v1

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
 import React from 'react';
 import { Route, Switch } from 'react-router-dom';
 import Login from './pages/Login';
+import Wallet from './pages/Wallet'
 
 class App extends React.Component {
   render() {
     return (
-      <div>
-        <Switch>
-          <Route exact path="/" component={ Login } />
-        </Switch>
-      </div>
+      <Switch>
+        <Route exact path="/" component={ Login } />
+        <Route exact path="/carteira" component={ Wallet } />
+      </Switch>
     );
   }
 }
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,11 +1,105 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import { login } from '../actions';
 
 class Login extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      email: '',
+      password: '',
+    };
+
+    this.handleChange = this.handleChange.bind(this);
+    this.validSubmit = this.validSubmit.bind(this);
+    this.eventClick = this.eventClick.bind(this);
+  }
+
+  handleChange(event) {
+    const { target } = event;
+    const { name, value } = target;
+    this.setState({
+      [name]: value,
+    });
+  }
+
+  // Validation script by Layo
+  eventClick() {
+    const { history, getEmail } = this.props;
+    const { email } = this.state;
+    getEmail(email);
+    if (this.validSubmit()) history.push('/carteira');
+  }
+
+  isValidEmail(email) {
+    const valid = email.match(/^([\w.-]+)@([\w-]+)((.(\w){2,3})+)$/);
+    return valid != null;
+  }
+
+  isValidPassword(password) {
+    const minimumCharacter = 6;
+    return password.length >= minimumCharacter;
+  }
+
+  validSubmit() {
+    const { password, email } = this.state;
+    return this.isValidEmail(email) && this.isValidPassword(password);
+  }
+  // ----
+
   render() {
+    const { email, password } = this.state;
     return (
-      <div>login</div>
+      <div>
+        <form>
+          <label htmlFor="user-email">
+            E-mail:
+            <input
+              data-testid="email-input"
+              placeholder="Digite seu email"
+              id="user-email"
+              type="text"
+              name="email"
+              value={ email }
+              onChange={ this.handleChange }
+            />
+          </label>
+          <label htmlFor="user-password">
+            Senha:
+            <input
+              data-testid="password-input"
+              placeholder="Digite sua senha"
+              id="user-password"
+              type="password"
+              name="password"
+              value={ password }
+              onChange={ this.handleChange }
+            />
+          </label>
+          <button
+            type="submit"
+            onClick={ this.eventClick }
+            disabled={ !this.validSubmit() }
+          >
+            Entrar
+          </button>
+        </form>
+      </div>
     );
   }
 }
 
-export default Login;
+Login.propTypes = {
+  getEmail: PropTypes.func.isRequired,
+  history: PropTypes.shape({
+    push: PropTypes.func.isRequired,
+  }).isRequired,
+};
+
+const mapDispatchToProps = (dispatch) => ({
+  getEmail: (email) => dispatch(login(email)),
+});
+
+export default connect(null, mapDispatchToProps)(Login);
