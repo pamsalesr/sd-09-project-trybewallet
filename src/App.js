d1acfe14626f93e91f0ef2cfccf25a5936b60f08
LucioOSilva <lucioosilva00@gmail.com>
2021-03-29 16:28:04 -0300

    Added Redux standard configs

--- src/App.js
+++ src/App.js
@@ -1,7 +1,16 @@
 import React from 'react';
+import { Route, Switch } from 'react-router-dom';
+import Login from './pages/Login';
+import NotFound from './pages/NotFound';
+
 
 function App() {
-  return <div>Hello, TrybeWallet!</div>;
+  return (
+    <Switch>
+      <Route exact path="/" component={ Login } />
+      <Route component={ NotFound } />
+    </Switch>
+  );
 }
 
 export default App;
