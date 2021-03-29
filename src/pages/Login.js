1b7aa05474cbedd6c9b673e5b52fe2d4e52efafd
Micaela Alves <micaela.alves93@gmail.com>
2021-03-29 17:53:25 -0300

    Input Email/Senha e Botão Entrar

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -2,7 +2,22 @@ import React from 'react';
 
 class Login extends React.Component {
   render() {
-    return <div>Login</div>;
+    return(
+      <main>
+        <h3>Login</h3>
+        <input
+          type="email"
+          placeholder="Endereço de e-mail"
+          data-testid="email-input"
+        />
+        <input
+          type="password"
+          placeholder="Senha"
+          data-testid="password-input"
+        />
+        <button type="submit">Entrar</button>
+      </main>
+    );
   }
 }
 
