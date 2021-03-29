c564599efdf9f3361ef25ab8620184a2f1735a37
Lucas Silva <martssilvapro@gmail.com>
2021-03-29 18:56:39 -0300

    header e formularios feitos

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -2,7 +2,21 @@ import React from 'react';
 
 class Login extends React.Component {
   render() {
-    return <div>Login</div>;
+    return (
+      <div>
+        <form>
+          <label htmlFor="Email">
+            Email:
+            <input type="text" name="email" data-testid="email-input" />
+          </label>
+          <label htmlFor="Senha">
+            Senha:
+            <input type="text" name="senha" data-testid="password-input" />
+          </label>
+        </form>
+        <button type="button">Entrar</button>
+      </div>
+    );
   }
 }
 
--- src/pages/Wallet.js
+++ src/pages/Wallet.js
@@ -1,8 +1,27 @@
 import React from 'react';
+import Form from '../components/formDespesa';
 
 class Wallet extends React.Component {
   render() {
-    return <div>TrybeWallet</div>;
+    return (
+      <div>
+        <header>
+          <section>
+            <div data-testid="email-field">Email: </div>
+            <div data-testid="total-field">
+              Despesa Total: 0
+              <div data-testid="header-currency-field">
+                BRL
+              </div>
+            </div>
+          </section>
+        </header>
+        <section>
+          <Form />
+          <button type="button">Adicionar despesa</button>
+        </section>
+      </div>
+    );
   }
 }
 
