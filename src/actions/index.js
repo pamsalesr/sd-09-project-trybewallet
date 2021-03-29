ac61d38fce570a98e3334b116d514a33f6c99932
RodrigoNeoEx <draja.acc@gmail.com>
2021-03-29 19:33:05 -0300

    conected wallet to get email from store, req 3

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,4 +1,7 @@
-// Coloque aqui suas actions
 export const LOGIN = 'LOGIN';
 
 export const login = (email) => ({ type: LOGIN, email });
+
+// export const Wallet = 'Wallet';
+
+// export const wallet = (currency) => ({ type: Wallet, currency });
--- src/pages/Wallet.js
+++ src/pages/Wallet.js
@@ -1,9 +1,36 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import { string } from 'prop-types';
 
 class Wallet extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { total: 0 };
+  }
+
   render() {
-    return <div>TrybeWallet</div>;
+    const { total } = this.state;
+    const { email } = this.props;
+    return (
+      <div>
+        <header>
+          <p data-testid="email-field">
+            { `Hello user ${email}` }
+          </p>
+          <p data-testid="total-field">{`You have total of ${total}`}</p>
+          <p data-testid="header-currency-field">BRL in your account</p>
+        </header>
+      </div>
+    );
   }
 }
 
-export default Wallet;
+const mapStatetoProps = (state) => ({
+  email: state.user.email,
+});
+
+Wallet.propTypes = {
+  email: string,
+}.isRequired;
+
+export default connect(mapStatetoProps)(Wallet);
