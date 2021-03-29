768f7328ae792d5a6f1578aa66997333b2edbb1b
ggaldino95 <ggaldino95@gmail.com>
2021-03-29 18:55:04 -0300

    [Task 1] Login page successfully implemented with Redux

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,1 +1,4 @@
 // Coloque aqui suas actions
+const userLogin = (email) => ({ type: 'LOGIN', email });
+
+export default userLogin;
--- src/reducers/user.js
+++ src/reducers/user.js
@@ -1,1 +1,17 @@
 // Esse reducer será responsável por tratar as informações da pessoa usuária
+const INITIAL_STATE = {
+  email: '',
+};
+
+const LOGIN = 'LOGIN';
+
+const user = (state = INITIAL_STATE, { email, type }) => {
+  switch (type) {
+  case LOGIN:
+    return { email };
+  default:
+    return state;
+  }
+};
+
+export default user;
