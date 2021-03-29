e0c696d4f2adac3fa64921e453d702aa82668a0c
J.Henrique <jhenriqueribeirodesa@gmail.com>
2021-03-29 19:33:35 -0300

    Adicionar header da carteira

--- src/pages/Wallet.js
+++ src/pages/Wallet.js
@@ -1,9 +1,34 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
 
 class Wallet extends React.Component {
   render() {
-    return <div>TrybeWallet</div>;
+    const { email, totalExpenses, currentCurrency } = this.props;
+    return (
+      <div>
+        <header>
+          <h3 data-testid="email-field">{email}</h3>
+          <h3 data-testid="total-field">
+            {totalExpenses}
+            {' '}
+            <span data-testid="header-currency-field">{currentCurrency}</span>
+          </h3>
+        </header>
+      </div>);
   }
 }
 
-export default Wallet;
+Wallet.propTypes = {
+  email: PropTypes.string.isRequired,
+  totalExpenses: PropTypes.number.isRequired,
+  currentCurrency: PropTypes.number.isRequired,
+};
+
+const mapStateToProps = (state) => ({
+  email: state.user.email,
+  totalExpenses: 0,
+  currentCurrency: 'BRL',
+});
+
+export default connect(mapStateToProps)(Wallet);
