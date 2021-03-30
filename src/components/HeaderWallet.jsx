ebb82e49c84e5ce8cb7df73b9b886cfd3f41c13f
Aurelio <aurelioscarante25@gmail.com>
2021-03-30 18:18:30 -0300

    :rocket: Requisitos 2 e 3, criando a página Wallet e seus campos

---
+++ src/components/HeaderWallet.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import { connect } from 'react-redux';
+
+class HeaderWallet extends React.Component {
+  render() {
+    const { email } = this.props;
+    return (
+      <header>
+        <div>
+          <strong>Email:</strong>
+          <span data-testid="email-field">{ email }</span>
+          <strong>Despesa Total:</strong>
+          <span data-testid="total-field">R$ 0,00</span>
+          <span data-testid="header-currency-field">BRL</span>
+        </div>
+      </header>
+    );
+  }
+}
+
+const mapStateToProps = (state) => ({
+  email: state.user.email,
+});
+
+export default connect(mapStateToProps)(HeaderWallet);
