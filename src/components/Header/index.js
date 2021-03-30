89bcb14adfffda5ca6736d3665883732055a35e8
Demistocles <demi.treze@gmail.com>
2021-03-30 16:28:03 -0300

    resolvendo erro de lint

---
+++ src/components/Header/index.js
@@ -0,0 +1,42 @@
+import react from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import React from 'react';
+
+class Header extends React.Component {
+  render() {
+    const { email } = this.props;
+    return (
+      <header>
+        <div>
+          <h2>TrybeWallet</h2>
+        </div>
+        <div>
+          <div>
+            <p data-testid="email-field" >{email}</p>
+          </div>
+          <div>
+            <p>
+              Despesa total:
+              R$
+              <span data-testid="total-field" >0</span>
+            </p>
+          </div>
+          <div>
+            <p data-testid="header-currency-field" >BRL</p>
+          </div>
+        </div>
+      </header>
+    );
+  }
+}
+
+Header.propTypes = {
+  email: PropTypes.string.isRequired,
+};
+
+const mapStateToProps = (state) => ({
+  email: state.user.email,
+});
+
+export default connect(mapStateToProps, null)(Header);
