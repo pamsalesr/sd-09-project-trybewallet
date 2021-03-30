67f78a0322ef822f44b28eca8911aa7de2d7878d
Maurice Alves <maurice.estevam@gmail.com>
2021-03-30 16:06:26 -0300

    Requisito - 1.

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,1 +1,9 @@
 // Coloque aqui suas actions
+import { LOGIN } from '../store/constantes';
+
+const loginAct = (email) => ({
+  type: LOGIN,
+  payload: email,
+});
+
+export default loginAct;
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,9 +1,102 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import { Redirect } from 'react-router-dom';
+import loginAction from '../actions';
 
 class Login extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      email: '',
+      password: '',
+      disable: true,
+      redirect: false,
+    };
+
+    this.loginUser = this.loginUser.bind(this);
+    this.handleChanges = this.handleChanges.bind(this);
+    this.enable = this.enable.bind(this);
+  }
+
+  handleChanges(event) {
+    const inputValue = event.target.value;
+    this.setState({
+      [event.target.id]: inputValue,
+    }, this.enable);
+  }
+
+  validationEmail(email) {
+    const regexEmail = new RegExp([
+      '^(([^<>()[\\]\\.,;:\\s@\\"]+(\\.[^<>()',
+      '[\\]\\.,;:\\s@\\"]+)*)|(\\".+\\"))@(([^<>()',
+      '[\\]\\.,;:\\s@\\"]+\\.)+[^<>()[\\]\\.,;:\\s@\\"]{2,})$',
+    ].join(''));
+    return regexEmail.test(email);
+  }
+
+  enable() {
+    const { email, password } = this.state;
+    const NUMBER = 6;
+    if (password.length >= NUMBER && this.validationEmail(email)) {
+      this.setState({
+        disable: false,
+      });
+    } else {
+      this.setState({
+        disable: true,
+      });
+    }
+  }
+
+  loginUser(param) {
+    param.preventDefault();
+    const { loginWithEmail } = this.props;
+    const { email } = this.state;
+    loginWithEmail(email);
+    this.setState({
+      redirect: true,
+    });
+  }
+
   render() {
-    return <div>Login</div>;
+    const { disable, redirect } = this.state;
+    return redirect
+      ? <Redirect to="/carteira" />
+      : (
+        <form onSubmit={ this.loginUser }>
+          <input
+            id="email"
+            data-testid="email-input"
+            type="email"
+            placeholder="Email"
+            onChange={ this.handleChanges }
+          />
+          <input
+            id="password"
+            data-testid="password-input"
+            type="password"
+            placeholder="Senha"
+            onChange={ this.handleChanges }
+          />
+          <button
+            type="submit"
+            disabled={ disable }
+          >
+            Entrar
+          </button>
+        </form>
+      );
   }
 }
 
-export default Login;
+const mapDispatchToProps = (dispatch) => ({
+  loginWithEmail: (email) => dispatch(loginAction(email)),
+});
+
+Login.propTypes = {
+  loginWithEmail: PropTypes.func.isRequired,
+};
+
+export default connect(null, mapDispatchToProps)(Login);
