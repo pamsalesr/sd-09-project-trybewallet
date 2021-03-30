27e95fc6d3bd120ea518450e2c0c8bb49e509e2b
J.Henrique <jhenriqueribeirodesa@gmail.com>
2021-03-30 10:24:01 -0300

    Adiciona total de despesas e formulario de adicionar despesa

--- src/pages/Wallet.js
+++ src/pages/Wallet.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
+import FormAddExpense from '../components/FormAddExpense';
 
 class Wallet extends React.Component {
   render() {
@@ -9,12 +10,13 @@ class Wallet extends React.Component {
       <div>
         <header>
           <h3 data-testid="email-field">{email}</h3>
-          <h3 data-testid="total-field">
-            {totalExpenses}
+          <h3>
+            <span data-testid="total-field">{totalExpenses}</span>
             {' '}
             <span data-testid="header-currency-field">{currentCurrency}</span>
           </h3>
         </header>
+        <FormAddExpense />
       </div>);
   }
 }
@@ -22,12 +24,18 @@ class Wallet extends React.Component {
 Wallet.propTypes = {
   email: PropTypes.string.isRequired,
   totalExpenses: PropTypes.number.isRequired,
-  currentCurrency: PropTypes.number.isRequired,
+  currentCurrency: PropTypes.string.isRequired,
 };
 
 const mapStateToProps = (state) => ({
   email: state.user.email,
-  totalExpenses: 0,
+  totalExpenses: state.wallet.expenses.reduce(
+    (prev, curr) => {
+      const currencyValue = parseFloat(curr.exchangeRates[curr.currency].ask);
+      const convertedValue = parseFloat(curr.value) * currencyValue;
+      return prev + convertedValue;
+    }, 0,
+  ),
   currentCurrency: 'BRL',
 });
 
