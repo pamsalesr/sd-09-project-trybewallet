67039e297795cad2ea1b1a07cce9cdf103e87e41
Enio Nicael <enionicael@gmail.com>
2021-03-29 21:18:23 -0300

    Requisito 1 implementa um formulario na pagina de login.

--- src/App.js
+++ src/App.js
@@ -1,7 +1,19 @@
 import React from 'react';
+import { Switch, Route } from 'react-router-dom';
 
-function App() {
-  return <div>Hello, TrybeWallet!</div>;
+import Login from './pages/Login';
+
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+        <Switch>
+          <Route exact path="/" component={ Login } />
+        </Switch>
+      </div>
+    );
+
+  }
 }
 
 export default App;
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,8 +1,20 @@
 import React from 'react';
+import { MdAccountCircle } from 'react-icons/md';
+
+import './Login.css';
 
 class Login extends React.Component {
   render() {
-    return <div>Login</div>;
+    return (
+      <div className="container-login">
+        <form className="form-login" action="">
+          <MdAccountCircle size={ 80 } />
+          <input type="email" name="email" id="email" placeholder="E-mail" data-testid="email-input" />
+          <input type="password" name="password" id="password" placeholder="Senha" data-testid="password-input" />
+          <button type="button" >Enviar</button>
+        </form>
+      </div>
+    );
   }
 }
 
