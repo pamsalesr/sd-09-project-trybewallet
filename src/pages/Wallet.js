b6a949bd8cdaef711fc757bced8c599c5caa31c8
Renato Feitosa <pythaqua@gmail.com>
2021-03-30 18:01:02 -0300

    feat(Wallet): Add 2nd & 3rd requirements

--- src/pages/Wallet.js
+++ src/pages/Wallet.js
@@ -1,9 +1,33 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
 
 class Wallet extends React.Component {
   render() {
-    return <div>TrybeWallet</div>;
+    const { email } = this.props;
+
+    return (
+      <>
+        <header>
+          <div>TrybeWallet</div>
+          <span data-testid="email-field">{ email }</span>
+          <span data-testid="total-field">{ 0 }</span>
+          <span data-testid="header-currency-field">BRL</span>
+        </header>
+        <div>a</div>
+      </>
+    );
   }
 }
 
-export default Wallet;
+const mapStateToProps = ({ user: { email } }) => ({ email });
+
+export default connect(mapStateToProps)(Wallet);
+
+Wallet.propTypes = {
+  email: PropTypes.string,
+};
+
+Wallet.defaultProps = {
+  email: 'Não identificado',
+};
