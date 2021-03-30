95c54f223dea8811014d6a656759e097a37eefb8
Demistocles <demi.treze@gmail.com>
2021-03-30 16:32:27 -0300

    corrigindo erros

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,7 +1,6 @@
-import react from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
-import React from 'react';
 
 class Header extends React.Component {
   render() {
@@ -13,17 +12,17 @@ class Header extends React.Component {
         </div>
         <div>
           <div>
-            <p data-testid="email-field" >{email}</p>
+            <p data-testid="email-field">{email}</p>
           </div>
           <div>
             <p>
               Despesa total:
               R$
-              <span data-testid="total-field" >0</span>
+              <span data-testid="total-field">0</span>
             </p>
           </div>
           <div>
-            <p data-testid="header-currency-field" >BRL</p>
+            <p data-testid="header-currency-field">BRL</p>
           </div>
         </div>
       </header>
