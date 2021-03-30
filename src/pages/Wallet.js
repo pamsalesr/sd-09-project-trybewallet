67e4b20c8c40405c665df0eebf46eecc2b9cc221
fomes <contato.felipeluna@gmail.com>
2021-03-30 15:50:24 -0300

    fix wallet eslint

--- src/pages/Wallet.js
+++ src/pages/Wallet.js
@@ -122,9 +122,9 @@ class Wallet extends Component {
                 onChange={ (e) => this.handleInputChange(e.target) }
               >
                 <option disabled value="default"> Selecione </option>
-                <option value="Dinheiro"> Dinheiro </option>
+                {/* <option value="Dinheiro"> Dinheiro </option>
                 <option value="Cartão de crédito"> Cartão de crédito </option>
-                <option value="Cartão de débito"> Cartão de débito </option>
+                <option value="Cartão de débito"> Cartão de débito </option> */}
               </select>
             </label>
             <label htmlFor="tag">
@@ -136,8 +136,8 @@ class Wallet extends Component {
                 defaultValue="default"
                 onChange={ (e) => this.handleInputChange(e.target) }
               >
-                {/* <option disabled value="default"> Selecione </option>
-                <option value="Alimentação"> Alimentação </option>
+                <option disabled value="default"> Selecione </option>
+                {/* <option value="Alimentação"> Alimentação </option>
                 <option value="Lazer"> Lazer </option>
                 <option value="Trabalho"> Trabalho </option>
                 <option value="Transporte"> Transporte </option> */}
