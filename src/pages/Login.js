8a89a8ea237324fdf83f78bfb2630848640f6cc3
Igor Marcos Riegel <igor.riegel@gmail.com>
2021-03-30 03:30:23 -0300

    habilitado o redirecionamento da pagina login para carteira, inicializados alguns estados e feitos fixes de style.

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,21 +1,67 @@
 import React from 'react';
-import './Login.css'
+import './Login.css';
 
 class Login extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      user: {
+        email: '',
+      },
+      loginDisabled: false,
+      invalidEmail: true,
+      invalidPass: true,
+    };
+    this.validateFields = this.validateFields.bind(this);
+    this.redirectPage = this.redirectPage.bind(this);
+  };
+
+  validateFields() {
+    const rgxEmail = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/g;
+  }
+
+  redirectPage() {
+    this.props.history.push("/carteira");
+  }
+
+  showStuff() {
+
+  }
+
   render() {
+    const { loginDisabled, invalidEmail, invalidPass } = this.state;
     return (
       <main>
-        <h1>Trybe Wallet</h1>
-        <form action="">
-          <label for="user">
-            user:
-            <input data-testid="email-input" type="text" name="user" id="" />
+        <form>
+          <h1>Trybe Wallet</h1>
+          <label htmlFor="user">
+            usuário:
+            <input
+              data-testid="email-input"
+              type="text"
+              name="user"
+              onChange={ this.validateFields }
+            />
+            {invalidEmail && <span className="invalid">Insira um email valido</span>}
           </label>
-          <label for="password">
-            password:
-            <input data-testid="password-input" type="text" name="password" id="" />
+          <label htmlFor="password">
+            senha:
+            <input
+              data-testid="password-input"
+              type="text"
+              name="password"
+              onChange={ this.validateFields }
+            />
+            {invalidEmail
+              && <span className="invalid">Senha de pelo menos 6 caracteres</span>}
           </label>
-          <button type="submit">Entrar</button>
+          <button
+            type="button"
+            disabled={ loginDisabled }
+            onClick={ () => { this.redirectPage(); } }
+          >
+            Entrar
+          </button>
         </form>
       </main>
     );
