0b0f582560206a76283dd4208268e02797970881
Rafael Pufal <rafael.pufal23@gmail.com>
2021-03-30 12:50:21 -0300

    Requisito 3

--- src/pages/Wallet.js
+++ src/pages/Wallet.js
@@ -3,12 +3,23 @@ import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 
 class Wallet extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      totalExpenses: 0,
+      currency: 'BRL',
+    };
+  }
+
   render() {
     const { emailInfo } = this.props;
+    const { totalExpenses, currency } = this.state;
     return (
       <header>
-        TrybeWallet teste
-        <p>{ emailInfo }</p>
+        TrybeWallet
+        <p data-testid="email-field">{ emailInfo }</p>
+        <p data-testid="total-field">{totalExpenses}</p>
+        <p data-testid="header-currency-field">{currency}</p>
       </header>
     );
   }
