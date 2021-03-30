c43890fba127d4350fc51bb7d6938c5cafda0d00
Carine Becker <caarine.m@gmail.com>
2021-03-30 15:34:28 -0300

    req 1

--- src/index.js
+++ src/index.js
@@ -1,16 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
 import './index.css';
-// import App from './App';
-import Login from './pages/Login';
+import App from './App';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
   <BrowserRouter>
-    <Switch>
-      <Route path="/" component={ Login } />
-    </Switch>
+    <App />
   </BrowserRouter>,
   document.getElementById('root'),
 );
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 class Login extends React.Component {
   constructor() {
@@ -6,28 +7,54 @@ class Login extends React.Component {
 
     this.state = {
       email: '',
+      isFilled: false,
     };
+
+    this.handleChange = this.handleChange.bind(this);
+    this.verifyLoginFields = this.verifyLoginFields.bind(this);
+  }
+
+  verifyLoginFields() {
+    const { email } = this.state;
+    return email === ''
+      ? this.setState({ isFilled: false })
+      : this.setState({ isFilled: true });
+  }
+
+  handleChange(event) {
+    const { name, value } = event.target;
+    this.setState({ [name]: value });
   }
 
   render() {
+    const { isFilled } = this.state;
     return (
       <main className="login-container">
         <h2>Login</h2>
         <form className="login-fields">
           <input
             type="email"
+            name="email"
             data-testid="email-input"
             placeholder="Digite seu e-mail"
             required
+            onChange={ this.handleChange }
           />
           <input
             type="password"
             data-testid="password-input"
-            min="6"
+            minLength="6"
             placeholder="Crie sua senha"
             required
+            onChange={ this.verifyLoginFields }
           />
-          <button type="submit">Entrar</button>
+          <Link to="/carteira">
+            {
+              isFilled
+                ? <button type="submit">Entrar</button>
+                : <button type="submit" disabled>Entrar</button>
+            }
+          </Link>
         </form>
       </main>
     );
