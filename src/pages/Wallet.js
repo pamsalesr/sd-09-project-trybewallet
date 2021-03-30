3609a10c436cb75b0bb2a9200de8372fb0c07b6c
ggaldino95 <ggaldino95@gmail.com>
2021-03-29 21:00:04 -0300

    [Task 3] Header section successfully implemented with Redux

--- src/pages/Wallet.js
+++ src/pages/Wallet.js
@@ -1,9 +1,38 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
 
 class Wallet extends React.Component {
   render() {
-    return <div>TrybeWallet</div>;
+    const { email } = this.props;
+    return (
+      <main>
+        <header>
+          <span data-testid="email-field">
+            Email:
+            { email }
+          </span>
+          <span data-testid="total-field">
+            Despesa Total:
+            0
+          </span>
+          <span data-testid="header-currency-field">
+            BRL
+          </span>
+        </header>
+      </main>
+    );
   }
 }
 
-export default Wallet;
+Wallet.propTypes = {
+  email: PropTypes.string,
+};
+
+Wallet.defaultProps = {
+  email: '',
+};
+
+const mapStateToProps = ({ user: { email } }) => ({ email }); // O parâmetro de mapStateToProps é o reducer do usuario (user) desestruturado para pegar a chave email.
+
+export default connect(mapStateToProps, null)(Wallet);
