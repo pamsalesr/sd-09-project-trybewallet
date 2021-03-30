7ea5f64c519a51b2e04a9e66921dbf54ac8b7612
fomes <contato.felipeluna@gmail.com>
2021-03-30 16:14:21 -0300

    eslint fix spaces

--- src/pages/Wallet.js
+++ src/pages/Wallet.js
@@ -85,83 +85,83 @@ class Wallet extends Component {
           </span>
           <span data-testid="header-currency-field"> BRL </span>
         </header>
-          <form>
-            <label htmlFor="value">
-              <span>Valor: </span>
-              <input
-                data-testid="value-input"
-                type="number"
-                name="value"
-                id="value"
-                value={ value }
-                onChange={ (e) => this.handleInputChange(e.target) }
-              />
-            </label>
-            <label htmlFor="currency">
-              <span> Moeda: </span>
-              <select
-                data-testid="currency-input"
-                name="currency"
-                id="currency"
-                defaultValue="default"
-                onChange={ (e) => this.handleInputChange(e.target) }
-              >
-                {/*  */}
-                <option disabled value="default"> Selecione </option>
-                {cSelect}
-              </select>
-            </label>
-            <label htmlFor="method-input">
-              <span>Método de Pagamento: </span>
-              <select
-                data-testid="method-input"
-                name="method"
-                id="method-input"
-                defaultValue="default"
-                onChange={ (e) => this.handleInputChange(e.target) }
-              >
-                <option disabled value="default"> Selecione </option>
-                <option value="Dinheiro"> Dinheiro </option>
-                <option value="Cartão de crédito"> Cartão de crédito </option>
-                <option value="Cartão de débito"> Cartão de débito </option>
-              </select>
-            </label>
-            <label htmlFor="tag">
-              <span>Tag: </span>
-              <select
-                data-testid="tag-input"
-                name="tag"
-                id="tag"
-                defaultValue="default"
-                onChange={ (e) => this.handleInputChange(e.target) }
-              >
-                <option disabled value="default"> Selecione </option>
-                <option value="Alimentação"> Alimentação </option>
-                <option value="Lazer"> Lazer </option>
-                <option value="Trabalho"> Trabalho </option>
-                <option value="Transporte"> Transporte </option>
-                {/*  */}
-                <option value="Saúde"> Saúde </option>
-              </select>
-            </label>
-            <label htmlFor="description">
-              <span>Descrição: </span>
-              <input
-                data-testid="description-input"
-                type="text"
-                name="description"
-                id="description"
-                value={ description }
-                onChange={ (e) => this.handleInputChange(e.target) }
-              />
-            </label>
-            <button
-              type="button"
-              onClick={ () => this.storeExp() }
+        <form>
+          <label htmlFor="value">
+            <span>Valor: </span>
+            <input
+              data-testid="value-input"
+              type="number"
+              name="value"
+              id="value"
+              value={ value }
+              onChange={ (e) => this.handleInputChange(e.target) }
+            />
+          </label>
+          <label htmlFor="currency">
+            <span> Moeda: </span>
+            <select
+              data-testid="currency-input"
+              name="currency"
+              id="currency"
+              defaultValue="default"
+              onChange={ (e) => this.handleInputChange(e.target) }
             >
-              Adicionar despesa
-            </button>
-          </form>
+              {/*  */}
+              <option disabled value="default"> Selecione </option>
+              {cSelect}
+            </select>
+          </label>
+          <label htmlFor="method-input">
+            <span>Método de Pagamento: </span>
+            <select
+              data-testid="method-input"
+              name="method"
+              id="method-input"
+              defaultValue="default"
+              onChange={ (e) => this.handleInputChange(e.target) }
+            >
+              <option disabled value="default"> Selecione </option>
+              <option value="Dinheiro"> Dinheiro </option>
+              <option value="Cartão de crédito"> Cartão de crédito </option>
+              <option value="Cartão de débito"> Cartão de débito </option>
+            </select>
+          </label>
+          <label htmlFor="tag">
+            <span>Tag: </span>
+            <select
+              data-testid="tag-input"
+              name="tag"
+              id="tag"
+              defaultValue="default"
+              onChange={ (e) => this.handleInputChange(e.target) }
+            >
+              <option disabled value="default"> Selecione </option>
+              <option value="Alimentação"> Alimentação </option>
+              <option value="Lazer"> Lazer </option>
+              <option value="Trabalho"> Trabalho </option>
+              <option value="Transporte"> Transporte </option>
+              {/*  */}
+              <option value="Saúde"> Saúde </option>
+            </select>
+          </label>
+          <label htmlFor="description">
+            <span>Descrição: </span>
+            <input
+              data-testid="description-input"
+              type="text"
+              name="description"
+              id="description"
+              value={ description }
+              onChange={ (e) => this.handleInputChange(e.target) }
+            />
+          </label>
+          <button
+            type="button"
+            onClick={ () => this.storeExp() }
+          >
+            Adicionar despesa
+          </button>
+        </form>
         <Table />
       </div>
     );
