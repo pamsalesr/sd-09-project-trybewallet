2595394752503ff67f9c5279dc395c494d8c06c2
vinicius <viniciuspalenske@gmail.com>
2021-03-31 17:57:23 -0300

    [fix][lint]Export login em pages/Login.js

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -2,7 +2,7 @@ import React from 'react';
 import Proptypes from 'prop-types';
 import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
-import { login } from '../actions';
+import login from '../actions';
 
 class Login extends React.Component {
   constructor() {
