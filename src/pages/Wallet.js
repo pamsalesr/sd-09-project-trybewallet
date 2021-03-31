b5400ea4f571f30cbf3b2640cf9aa83ce9eba99e
vinicius <viniciuspalenske@gmail.com>
2021-03-31 19:17:18 -0300

    entrega requisito 3

--- src/pages/Wallet.js
+++ src/pages/Wallet.js
@@ -1,9 +1,40 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import Proptypes from 'prop-types';
 
 class Wallet extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      spending: 0,
+      currency: 'BRL',
+    };
+  }
+
   render() {
-    return <div>TrybeWallet</div>;
+    const { email } = this.props;
+    const { spending, currency } = this.state;
+    return (
+      <div>
+        <header>
+          <div data-testid="email-field">
+            {email}
+          </div>
+          <div data-testid="total-field">
+            {`gastos: ${spending}`}
+          </div>
+          <div data-testid="header-currency-field">
+            {`moeda: ${currency}`}
+          </div>
+        </header>
+      </div>
+    );
   }
 }
 
-export default Wallet;
+const mapStateToProps = (state) => ({
+  email: state.user.email });
+
+Wallet.propTypes = { email: Proptypes.string.isRequired };
+
+export default connect(mapStateToProps, null)(Wallet);
