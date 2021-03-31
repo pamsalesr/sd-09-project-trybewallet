370cc1af9ab4aa97bac6b8a97437607d476707fd
Thiago Granville <thiagogranville@gmail.com>
2021-03-31 16:58:51 -0300

    Requisito 1: Cria página de login e suas funções básicas

--- src/App.js
+++ src/App.js
@@ -1,8 +1,15 @@
 import React, { Component } from 'react';
+import { Route, Switch } from 'react-router-dom';
+import { Login, Wallet } from './pages';
 
 class App extends Component {
   render() {
-    return <div>Hello, TrybeWallet!</div>;
+    return (
+      <Switch>
+        <Route exact path="/" component={ Login } />
+        <Route path="/carteira" component={ Wallet } />
+      </Switch>
+    );
   }
 }
 
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,9 +1,93 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+import loginAction from '../actions/loginAction';
 
 class Login extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      email: '',
+      password: '',
+      validEmail: false,
+      validPassword: false,
+    };
+
+    this.handleChange = this.handleChange.bind(this);
+    this.validateLogin = this.validateLogin.bind(this);
+  }
+
+  validateLogin(name, value) {
+    const emailRegex = /^[a-z0-9.]+@[a-z0-9]+\.[a-z]$/i;
+    const passwordMinLenght = 6;
+
+    if (name === 'email' && emailRegex.test(value)) {
+      this.setState({
+        validEmail: true,
+      });
+    }
+
+    if (name === 'password' && value.length >= passwordMinLenght) {
+      this.setState({
+        validPassword: true,
+      });
+    }
+  }
+
+  handleChange({ target }) {
+    const { name, value } = target;
+
+    this.setState({
+      [name]: value,
+    });
+
+    this.validateLogin(name, value);
+  }
+
   render() {
-    return <div>Login</div>;
+    const { email, password, validEmail, validPassword } = this.state;
+    const { dispatchLogin } = this.props;
+    const buttonStatus = validEmail && validPassword;
+    return (
+      <div>
+        <input
+          name="email"
+          type="email"
+          data-testid="email-input"
+          value={ email }
+          placeholder="e-mail"
+          onChange={ this.handleChange }
+        />
+        <input
+          name="password"
+          type="password"
+          data-testid="password-input"
+          value={ password }
+          placeholder="senha"
+          onChange={ this.handleChange }
+        />
+        <Link to="/carteira">
+          <button
+            type="button"
+            disabled={ !buttonStatus }
+            onClick={ dispatchLogin }
+          >
+            Entrar
+          </button>
+        </Link>
+      </div>
+    );
   }
 }
 
-export default Login;
+const mapDispatchToProps = (dispatch) => ({
+  dispatchLogin: (email) => dispatch(loginAction(email)),
+});
+
+Login.propTypes = {
+  dispatch: PropTypes.func,
+}.isRequired;
+
+export default connect(null, mapDispatchToProps)(Login);
