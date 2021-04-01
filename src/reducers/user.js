49bf809b8da72031d13c6ff63231e9f2656849b5
ViniciusChagas <viniciushrcs@gmail.com>
2021-04-01 19:23:41 -0300

    creating the redux flow

--- src/reducers/user.js
+++ src/reducers/user.js
@@ -1,1 +1,16 @@
-// Esse reducer será responsável por tratar as informações da pessoa usuária
+import { LOGIN } from '../actions';
+
+const INITIAL_STATE = {
+  email: '',
+};
+
+const user = (state = INITIAL_STATE, { email, type }) => {
+  switch (type) {
+  case LOGIN:
+    return { email };
+  default:
+    return state;
+  }
+};
+
+export default user;
