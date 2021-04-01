6594ab51e544b18a1ada565173c3ab411bc3cf9e
Emanoel Leão <emanuelleaomarinho@gmail.com>
2021-03-31 22:17:38 -0300

    Requiremet 03

--- src/pages/Wallet.js
+++ src/pages/Wallet.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
-import './Wallet.css';
 
 class Wallet extends React.Component {
   render() {
