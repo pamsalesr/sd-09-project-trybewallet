af5ad2e04268183bc7d5fafc8b0d21b3c5944064
Enio Nicael <enionicael@gmail.com>
2021-04-01 19:47:34 -0300

    Implementacao da estrutura do redux.

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,1 +1,9 @@
 // Coloque aqui suas actions
+import { REQUEST_LOGIN } from './actionTypes';
+
+const requestLogin = () => ({
+  type: REQUEST_LOGIN,
+  user: '',
+  password: '',
+  authorizedLogin: false,
+});
--- src/reducers/user.js
+++ src/reducers/user.js
@@ -1,1 +1,24 @@
 // Esse reducer será responsável por tratar as informações da pessoa usuária
+import { REQUEST_LOGIN } from '../actions/actionTypes';
+
+const INITIAL_USER_STATE = {
+  user: {
+    email: 'yoda@starwars.com',
+    password: '123456',
+    authorizedLogin: false,
+  }
+};
+
+const user = (state = INITIAL_USER_STATE, action) => {
+  switch (action.type) {
+    case REQUEST_LOGIN:
+      return {
+        ...state,
+        authorizedLogin: action.requestLogin,
+      }
+    default:
+      return state;
+  }
+}
+
+export default user;
