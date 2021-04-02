49e8354013bed24221edd98080c2d53f0b9cd2dc
Kamilavrhcp <kamilavrhcp@hotmail.com>
2021-04-02 20:28:52 -0300

    Feat: criar a pagina inicial

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,27 +1,66 @@
 import React from 'react';
-import { Button } from 'bootstrap';
-import { Link } from 'react-router-dom';
+import { Link, Redirect } from 'react-router-dom';
 
 class Login extends React.Component {
-  render() {
+  constructor(props) {
+    super(props);
+    this.state = {
+      email: '',
+      senha: '',
+      redirect: false,
+    };
+    this.inputsLogin = inputsLogin.bind(this);
+  }
+
+  verifyLogin() {
+    const { email, senha } = this.state;
+    const senhaLength = 6;
+    if (email === 'alguem@alguem.com' && senha >= senhaLength) {
+      this.setState({ redirect: true });
+    }
+  }
+
+  handleInputsChange({ targe: { name, value } }) {
+    this.setState({ [name]: value });
+  }
+
+  inputsLogin(labelName, id, name, ...params) {
+    const [type, valueName] = params;
     return (
-      <div>
-        <Link to="/">
-          <label htmlFor="email-input">
-            Email
-            <input
-              type="text"
-              data-testid="email-input"
-              placeholder="Insira o seu e-mail"
-            />
-          </label>
-          <label htmlFor="password-input">
-            Senha
-            <input type="password" data-testid="password-input" />
-          </label>
-          <Button>Entrar</Button>
-        </Link>
-      </div>
+      <label htmlFor={ id }>
+        {labelName}
+        <input
+          type={ type }
+          data-testid={ id }
+          value={ valueName }
+          name={ name }
+          onChange={ this.handleInputsChange }
+        />
+      </label>
+    );
+  }
+
+  render() {
+    const { redirect } = this.state;
+    if (redirect) return <Redirect to="/carteira" />;
+    return (
+      <forms>
+        <Link to="/" />
+        { this.inputsLogin(
+          'Email',
+          '"email-input"',
+          'email-input',
+          'email',
+          email,
+        )}
+        { this.inputsLogin(
+          'Senha',
+          '"password-input"',
+          'password-input', 'password',
+          password,
+        )}
+        <button type="button" onClick={ this.verifyLogin }>Entrar</button>
+      </forms>
     );
   }
 }
