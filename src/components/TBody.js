3bf1fb2b81df5eef5006fa85a4e1c6ca4a478206
Rafa Reis <rafaelrnascimento2006@gmail.com>
2021-04-01 22:33:12 -0300

    requisito 5 feito

--- src/components/TBody.js
+++ src/components/TBody.js
@@ -7,16 +7,18 @@ class TBody extends React.Component {
     const { expenses } = this.props;
     return (
       <tbody>
-        {expenses.map(({ value, description, currency, method, tag }, index) => (
+        {expenses.map((
+          { value, description, currency, method, tag, exchangeRates }, index,
+        ) => (
           <tr key={ index }>
             <td>{ description }</td>
             <td>{ tag }</td>
             <td>{ method }</td>
             <td>{ value }</td>
-            <td>{ currency }</td>
-            <td>placeHolder</td>
-            <td>placeHolder</td>
-            <td>placeHolder</td>
+            <td>{ exchangeRates[currency].name }</td>
+            <td>{ Math.round(100 * exchangeRates[currency].ask) / 100 }</td>
+            <td>{ Math.round(value * 100 * (exchangeRates[currency].ask)) / 100 }</td>
+            <td>Real</td>
             <td><button type="button" data-testid="delete-btn">deletar</button></td>
           </tr>
         ))}
