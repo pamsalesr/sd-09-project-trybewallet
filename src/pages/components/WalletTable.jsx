84c490fcebf6f199322154f7817bb99b67892185
Igor Marcos Riegel <igor.riegel@gmail.com>
2021-04-02 05:09:15 -0300

    Implementada a tabela de exibiçao das despesas do requisito 5

--- src/pages/components/WalletTable.jsx
+++ src/pages/components/WalletTable.jsx
@@ -1,42 +1,65 @@
 import React, { Component } from 'react';
-// import PropTypes from 'prop-types';
-// import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
 
 class WalletTable extends Component {
-  // constructor(props) {
-  //   super(props);
-  //   this.state = {
-  //     none: '',
-  //   };
-  // }
+  constructor(props) {
+    super(props);
+    this.renderTableRows = this.renderTableRows.bind(this);
+  }
+
+  renderTableRows() {
+    const { expenses } = this.props;
+    return expenses.map((item) => (
+      <tr key={ item.id }>
+        <td>{item.description}</td>
+        <td>{item.tag}</td>
+        <td>{item.method}</td>
+        <td>{parseFloat(item.value).toFixed()}</td>
+        <td>{item.exchangeRates[item.currency].name}</td>
+        <td>{parseFloat(item.exchangeRates[item.currency].ask).toFixed(2)}</td>
+        <td>
+          {parseFloat(item.exchangeRates[item.currency].ask * item.value).toFixed(2)}
+        </td>
+        <td>Real</td>
+        <td>
+          <button type="button" data-testid="delete-btn">Delete</button>
+          <button type="button" data-testid="edit-btn">Editar</button>
+        </td>
+      </tr>
+    ));
+  }
 
   render() {
     return (
       <table>
         <thead>
           <tr>
-            <th>a</th>
+            <th>Descrição</th>
+            <th>Tag</th>
+            <th>Método de pagamento</th>
+            <th>Valor</th>
+            <th>Moeda</th>
+            <th>Câmbio utilizado</th>
+            <th>Valor convertido</th>
+            <th>Moeda de conversão</th>
+            <th>Editar/Excluir</th>
           </tr>
         </thead>
         <tbody>
-          <tr>
-            <td>b</td>
-          </tr>
+          {this.renderTableRows()}
         </tbody>
       </table>
     );
   }
 }
 
-// const mapStateToProps = (state) => ({
-// });
+const mapStateToProps = (state) => ({
+  expenses: state.wallet.expenses,
+});
 
-// const mapDispatchToProps = (dispatch) => ({
-// });
+WalletTable.propTypes = {
+  expenses: PropTypes.arrayOf(Object).isRequired,
+};
 
-// WalletTable.propTypes = {
-// };
-
-// connect(mapStateToProps, mapDispatchToProps)(WalletTable)
-
-export default WalletTable;
+export default connect(mapStateToProps)(WalletTable);
