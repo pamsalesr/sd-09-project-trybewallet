8a719a81de94044d8112d01b7e1169d303f55fcd
Felipe Shcolnik <shcolnik@gmail.com.br>
2021-04-03 19:51:21 -0300

    task1 ESLINT

--- src/pages/Wallet.js
+++ src/pages/Wallet.js
@@ -1,9 +1,26 @@
 import React from 'react';
+import { connect } from 'react-redux';
 
 class Wallet extends React.Component {
   render() {
-    return <div>TrybeWallet</div>;
+    const { email } = this.props;
+    return (
+      <header>
+        <h4 data-testid="email-field">{ email }</h4>
+        <input
+          data-testid="total-field"
+          type="number"
+        />
+      </header>
+    );
   }
 }
+mapStateToProps = (state) => ({
+  email: state.user.email,
+});
 
-export default Wallet;
+Wallet.propTypes = {
+  email: PropTypes.string,
+}.isRequired;
+
+export default connect(mapStateToProps)(Wallet);
