f715fd5eb48839dde81577f38bb0cb993d57af64
Kamilavrhcp <kamilavrhcp@hotmail.com>
2021-04-04 16:13:23 -0300

    Fix: EsLint

--- src/pages/Wallet.js
+++ src/pages/Wallet.js
@@ -1,35 +1,35 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import { connect } from 'react-redux';
-import { saveEmail } from '../actions';
+// import React from 'react';
+// import { Link } from 'react-router-dom';
+// import { connect } from 'react-redux';
+// import { saveEmail } from '../actions';
 
-class Wallet extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      email: '',
-    };
-  }
+// class Wallet extends React.Component {
+//   constructor(props) {
+//     super(props);
+//     this.state = {
+//       email: '',
+//     };
+//   }
 
-  render() {
-    // const { userEmail } = this.props;
-    const { email } = this.state;
-    return (
-      <header>
-        <Link to="/carteira">Carteira</Link>
-        <section>
-          <div>
-            Email:
-            { () => userEmail(email) }
-          </div>
-        </section>
-      </header>
-    );
-  }
-}
+//   render() {
+//     // const { userEmail } = this.props;
+//     const { email } = this.state;
+//     return (
+//       <header>
+//         <Link to="/carteira">Carteira</Link>
+//         <section>
+//           <div>
+//             Email:
+//             { () => userEmail(email) }
+//           </div>
+//         </section>
+//       </header>
+//     );
+//   }
+// }
 
-const mapDispatchToProps = (dispatch) => ({
-  userEmail: (email) => dispatch(saveEmail(email)),
-});
+// const mapDispatchToProps = (dispatch) => ({
+//   userEmail: (email) => dispatch(saveEmail(email)),
+// });
 
-export default connect(null, mapDispatchToProps)(Wallet);
+// export default connect(null, mapDispatchToProps)(Wallet);
