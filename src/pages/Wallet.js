ff6444a3219439b967ff4af91984145c687a5877
Kamilavrhcp <kamilavrhcp@hotmail.com>
2021-04-04 16:08:33 -0300

    Fix: EsLint

--- src/pages/Wallet.js
+++ src/pages/Wallet.js
@@ -1,14 +1,35 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { connect } from 'react-redux';
+import { saveEmail } from '../actions';
 
 class Wallet extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      email: '',
+    };
+  }
+
   render() {
+    const { userEmail } = this.props;
+    const { email } = this.state;
     return (
       <header>
         <Link to="/carteira">Carteira</Link>
+        <section>
+          <div>
+            Email:
+            { () => userEmail(email) }
+          </div>
+        </section>
       </header>
     );
   }
 }
 
-export default Wallet;
+const mapDispatchToProps = (dispatch) => ({
+  userEmail: (email) => dispatch(saveEmail(email)),
+});
+
+export default connect(null, mapDispatchToProps)(Wallet);
