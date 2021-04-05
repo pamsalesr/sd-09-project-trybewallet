4c8ae4cc944b8e341d1ffec4671291082bcd17d7
Layo Kaminski <layokaminski@gmail.com>
2021-04-05 12:37:39 -0300

    Requisito 6

--- src/reducers/wallet.js
+++ src/reducers/wallet.js
@@ -1,5 +1,11 @@
 // Esse reducer será responsável por tratar o todas as informações relacionadas as despesas
-import { AGROUP_CURRENCIES, ADD_EXPENSE, SUM_EXPENSES } from '../actions';
+import {
+  AGROUP_CURRENCIES,
+  ADD_EXPENSE,
+  SUM_EXPENSES,
+  DELETE_EXPENSE,
+  SUBTRACT_EXPENSES,
+} from '../actions';
 
 const INITIAL_STATE = {
   currencies: [],
@@ -15,6 +21,13 @@ const wallet = (state = INITIAL_STATE, action) => {
     return { ...state, expenses: [...state.expenses, ...action.payload] };
   case SUM_EXPENSES:
     return { ...state, total: parseFloat((state.total + action.payload).toFixed(2)) };
+  case DELETE_EXPENSE:
+    return {
+      ...state,
+      expenses: state.expenses.filter((expense) => expense.id !== action.payload),
+    };
+  case SUBTRACT_EXPENSES:
+    return { ...state, total: parseFloat((state.total - action.payload).toFixed(2)) };
   default:
     return state;
   }
