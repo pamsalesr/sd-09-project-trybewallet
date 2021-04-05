8bc0fd994f34ca8de1479ca19a74a1db54f7e3fb
fernandacajueiro <fernandacajueiromachado@gmail.com>
2021-04-05 19:05:34 +0200

    Adds new requirement

---
+++ src/components/ExpensesTable.jsx
@@ -0,0 +1,57 @@
+import React from 'react';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+
+class ExpensesTable extends React.Component {
+  tableInfo() {
+    const { expenses } = this.props;
+    return expenses.map((expense) => (
+      <tr key={ expense.id }>
+        <td>{expense.description}</td>
+        <td>{expense.tag}</td>
+        <td>{expense.method}</td>
+        <td>{expense.value}</td>
+        <td>{expense.exchangeRates[expense.currency].name}</td>
+        <td>{parseFloat(expense.exchangeRates[expense.currency].ask).toFixed(2)}</td>
+        <td>
+          {(parseFloat(expense.exchangeRates[expense.currency].ask) * expense.value)
+            .toFixed(2)}
+        </td>
+        <td>Real</td>
+        <td>Excluir</td>
+      </tr>));
+  }
+
+  render() {
+    return (
+      <table width="100%">
+        <thead>
+          <tr>
+            <th>Descrição</th>
+            <th>Tag</th>
+            <th>Método de pagamento</th>
+            <th>Valor</th>
+            <th>Moeda</th>
+            <th>Câmbio utilizado</th>
+            <th>Valor convertido</th>
+            <th>Moeda de conversão</th>
+            <th>Editar/Excluir</th>
+          </tr>
+        </thead>
+        <tbody>
+          { this.tableInfo() }
+        </tbody>
+      </table>
+    );
+  }
+}
+
+const mapStateToProps = (state) => ({
+  expenses: state.wallet.expenses,
+});
+
+ExpensesTable.propTypes = {
+  expenses: PropTypes.arrayOf(PropTypes.object).isRequired,
+};
+
+export default connect(mapStateToProps)(ExpensesTable);
--- src/pages/Wallet.js
+++ src/pages/Wallet.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { connect } from 'react-redux';
 import Header from '../components/Header';
 import Form from '../components/Form';
+import ExpensesTable from '../components/ExpensesTable';
 
 class Wallet extends React.Component {
   render() {
@@ -9,6 +10,7 @@ class Wallet extends React.Component {
       <div>
         <Header />
         <Form />
+        <ExpensesTable />
       </div>
     );
   }
