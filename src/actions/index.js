7916b41167fbe6fab59e792535448d11affb280e
alailsonko <aalailson3@gmail.com>
2021-04-05 19:16:51 -0300

    walltet actions

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,6 +1,18 @@
 export const SIGNIN = '@TRYBEWALLET/LOGIN';
+export const SAVE_CURRENCIES = '@TRYBEWALLET/SAVE_CURRENCIES';
+export const SAVE_EXPENSES = '@TRYBEWALLET/SAVE_EXPENSE';
 
 export const signin = (email) => ({
   type: SIGNIN,
   payload: email,
+});
+
+export const saveCurrencies = (currencies) => ({
+  type: SAVE_CURRENCIES,
+  payload: currencies,
+});
+
+export const saveExpense = (expenses) => ({
+  type: SAVE_EXPENSES,
+  payload: expenses,
 });
