18a7e31acf8a38026cd2850c448482c685d05c0d
Vagner Pereira <sousa.vagner82@gmail.com>
2021-04-05 12:59:56 -0300

    requisito 04

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,12 +3,29 @@ import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 
 class Header extends React.Component {
+  constructor() {
+    super();
+    this.getTotalExpenses = this.getTotalExpenses.bind(this);
+  }
+
+  getTotalExpenses() {
+    const { expenses } = this.props;
+    const totalExpenses = expenses.reduce((total, each) => {
+      const { value, currency, exchangeRates } = each;
+      const rate = parseFloat(exchangeRates[currency].ask);
+      return total + parseFloat(value) * rate;
+    }, 0);
+    return totalExpenses.toFixed(2);
+  }
+
   render() {
     const { email } = this.props;
     return (
       <header>
-        <h3 data-testid="email-field">{ email }</h3>
-        <h3 data-testid="total-field">0</h3>
+        <h3 data-testid="email-field">{`Email: ${email}` }</h3>
+        <h3 data-testid="total-field">
+          {`Despesa total: R$ ${this.getTotalExpenses()}`}
+        </h3>
         <h3 data-testid="header-currency-field">BRL</h3>
       </header>
 
@@ -18,10 +35,12 @@ class Header extends React.Component {
 
 const mapStateToProps = (state) => ({
   email: state.user.email,
+  expenses: state.wallet.expenses,
 });
 
 export default connect(mapStateToProps)(Header);
 
 Header.propTypes = {
   email: PropTypes.string.isRequired,
+  expenses: PropTypes.arrayOf(Object).isRequired,
 };
