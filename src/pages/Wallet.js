a41713dad18bb65d35aa0a206e518ec6862b86ee
Nícolas Tozato <ntozato9@gmail.com>
2021-04-04 22:27:44 -0300

    requisito 3

--- src/pages/Wallet.js
+++ src/pages/Wallet.js
@@ -1,9 +1,38 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
 
 class Wallet extends React.Component {
   render() {
-    return <div>TrybeWallet</div>;
+    const { user, expenses, currency } = this.props;
+    return (
+      <div>
+        <header>
+          <h1>Wallet</h1>
+          <h4 data-testid="email-field">{user}</h4>
+          <span data-testid="total-field">
+            Despesa Total:
+            {' '}
+            {`${expenses}`}
+          </span>
+          {' '}
+          <span data-testid="header-currency-field">{currency}</span>
+        </header>
+      </div>
+    );
   }
 }
 
-export default Wallet;
+const mapStateToProps = (state) => ({
+  user: state.user.email,
+  expenses: state.wallet.expenses,
+  currency: state.wallet.currencies,
+});
+
+Wallet.propTypes = {
+  user: PropTypes.string.isRequired,
+  expenses: PropTypes.number.isRequired,
+  currency: PropTypes.string.isRequired,
+};
+
+export default connect(mapStateToProps)(Wallet);
--- src/reducers/wallet.js
+++ src/reducers/wallet.js
@@ -1,1 +1,15 @@
-// Esse reducer será responsável por tratar o todas as informações relacionadas as despesas
+const INITIAL_STATE = {
+  currencies: ['BRL'],
+  expenses: [0],
+};
+
+const walletReducer = (state = INITIAL_STATE, action) => {
+  switch (action.type) {
+  case '':
+    return state;
+  default:
+    return state;
+  }
+};
+
+export default walletReducer;
