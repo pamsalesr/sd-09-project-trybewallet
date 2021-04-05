f3a85792f2cb03522e07b872e0d2a3573a0698bf
Rafael Pufal <rafael.pufal23@gmail.com>
2021-04-05 17:40:26 -0300

    requisito 4

--- src/pages/Wallet.js
+++ src/pages/Wallet.js
@@ -1,26 +1,30 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
+import ExpensesForm from '../components/ExpensesForm';
 
 class Wallet extends React.Component {
   constructor() {
     super();
     this.state = {
-      totalExpenses: 0,
+      // totalExpenses: 0,
       currency: 'BRL',
     };
   }
 
   render() {
-    const { emailInfo } = this.props;
-    const { totalExpenses, currency } = this.state;
+    const { emailInfo, totalExpensesState } = this.props;
+    const { currency } = this.state;
     return (
-      <header>
-        TrybeWallet
-        <p data-testid="email-field">{ emailInfo }</p>
-        <p data-testid="total-field">{totalExpenses}</p>
-        <p data-testid="header-currency-field">{currency}</p>
-      </header>
+      <div>
+        <header>
+          <p>TrybeWallet</p>
+          <span data-testid="email-field">{ emailInfo }</span>
+          <span data-testid="total-field">{totalExpensesState || 0}</span>
+          <span data-testid="header-currency-field">{currency}</span>
+        </header>
+        <ExpensesForm />
+      </div>
     );
   }
 }
@@ -31,10 +35,12 @@ class Wallet extends React.Component {
 
 Wallet.propTypes = {
   emailInfo: PropTypes.string.isRequired,
+  totalExpensesState: PropTypes.number.isRequired,
 };
 
 const mapStateToProps = (state) => ({
   emailInfo: state.user.email,
+  totalExpensesState: state.wallet.totalExpenses,
 });
 
 export default connect(mapStateToProps, null)(Wallet);
--- src/reducers/wallet.js
+++ src/reducers/wallet.js
@@ -1,14 +1,27 @@
 const INITIAL_STATE = {
   currencies: [],
   expenses: [],
+  exchangeRate: {},
+  totalExpenses: 0,
 };
 
 const ADD_TO_WALLET = 'ADD_TO_WALLET';
+const REQUEST_CURRENCIES = 'REQUEST_CURRENCIES';
+const RECEIVE_CURRENCIES = 'RECEIVE_CURRENCIES';
+const SEND_INFOS = 'SEND_INFOS';
 
 function wallet(state = INITIAL_STATE, action) {
   switch (action.type) {
   case ADD_TO_WALLET:
-    return { ...state, expenses: action.expense };
+    return { ...state,
+      expenses: [...state.expenses, action.payload],
+      totalExpenses: state.totalExpenses + action.expense };
+  case SEND_INFOS:
+    return { ...state, exchangeRate: action.payload };
+  case RECEIVE_CURRENCIES:
+    return { ...state, currencies: action.payload };
+  case REQUEST_CURRENCIES:
+    return { ...state, asyncWorkin: action.payload };
   default:
     return state;
   }
