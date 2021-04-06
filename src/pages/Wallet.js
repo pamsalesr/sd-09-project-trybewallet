0751ae5c3c93e1806518d41ce531774fabcd44e0
alailsonko <aalailson3@gmail.com>
2021-04-06 07:27:42 -0300

    form wallet component

--- src/pages/Wallet.js
+++ src/pages/Wallet.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import HeaderWallet from '../components/HeaderWallet';
+// import ExpensesTable from '../components/ExpensesTable';
+import FormWallet from '../components/FormWallet';
 
 class Wallet extends React.Component {
   render() {
     return (
       <div>
         <HeaderWallet />
+        <FormWallet />
+        {/* <ExpensesTable /> */}
       </div>
     );
   }
