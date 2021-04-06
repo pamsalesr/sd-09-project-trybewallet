68c1d480740181606b6393bee72f57ad1fa18b29
MatheuzaoCarvalho <matheusvscarvalho@gmail.com>
2021-04-06 11:05:42 -0300

    Hopefully task 04 - 05 - 06 - ok

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,18 +2,42 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 
+import expenseType from '../types';
+
+import styles from '../styles/components/Header.module.css';
+
 class Header extends Component {
+  getTotalAmount() {
+    const { expenses } = this.props;
+    return expenses
+      .map(({ currency, value, exchangeRates }) => {
+        const currencyData = exchangeRates[currency];
+        const total = Number(value) * Number(currencyData.ask);
+        return total;
+      })
+      .reduce((acc, expense) => acc + expense, 0);
+  }
+
   render() {
     const { email } = this.props;
     return (
-      <header>
-        <dl>
-          <dt>Email</dt>
-          <dd data-testid="email-field">{ email }</dd>
-          <dt>Total</dt>
-          <dd data-testid="total-field">RS0</dd>
-          <dt>Câmbio</dt>
-          <dd data-testid="header-currency-field">BRL</dd>
+      <header className={ styles.header }>
+        <dl className={ styles.headerInfosContainer }>
+          <div className={ styles.headerInfo }>
+            <dt>Email</dt>
+            <dd data-testid="email-field">{ email }</dd>
+          </div>
+          <div className={ styles.headerInfo }>
+            <dt>Total</dt>
+            <dd data-testid="total-field">
+              RS
+              { parseFloat(this.getTotalAmount()).toFixed(2) }
+            </dd>
+          </div>
+          <div className={ styles.headerInfo }>
+            <dt>Câmbio</dt>
+            <dd data-testid="header-currency-field">BRL</dd>
+          </div>
         </dl>
       </header>
 
@@ -23,11 +47,15 @@ class Header extends Component {
 
 Header.propTypes = {
   email: PropTypes.string.isRequired,
+  expenses: PropTypes.arrayOf(expenseType).isRequired,
 };
 
-const mapStateToProps = ({ user }) => ({ email: user.email });
+const mapStateToProps = ({ user, wallet }) => ({
+  email: user.email,
+  expenses: wallet.expenses,
+});
 
 export default connect(mapStateToProps)(Header);
 
 // https://developer.mozilla.org/pt-BR/docs/Web/HTML/Element/dl
-// Definition List  <dl> seguidos de <dd>
+// Definition List  <dl> seguidos de <dd> dica PSimoes
