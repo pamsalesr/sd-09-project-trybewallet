9cf3f5e1c3195ffc430eed96ea62497b419371fe
MatheuzaoCarvalho <matheusvscarvalho@gmail.com>
2021-04-06 01:09:21 -0300

    Starting the Project! Let's Go

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 import React from 'react';
 
 function App() {
-  return <div>Hello, TrybeWallet!</div>;
+  return <div>Hello, TrybeWallet! Lets Go!</div>;
 }
 
 export default App;
+
+// testing git flow
