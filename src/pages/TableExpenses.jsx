72343d4d0da0c220d7b5b2776bf2550f77908e66
Felipe Shcolnik <shcolnik@gmail.com.br>
2021-04-06 13:40:00 -0300

    EsLint task4 afetr reinstall

---
+++ src/pages/TableExpenses.jsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import { connect } from 'react-redux';
+import { arrayOf } from 'prop-types';
+
+class TableExpenses extends React.Component {
+  render() {
+    const { expenses } = this.props;
+    return (
+      <session>
+        { expenses.map((
+          { value, description, currency, method, tag, exchangeRates }, index,
+        ) => (
+          <tr key={ index }>
+            <td>{ description }</td>
+            <td>{ tag }</td>
+            <td>{ method }</td>
+            <td>{ value }</td>
+            <td>{ exchangeRates[currency].name }</td>
+            <td>{ Math.round(100 * exchangeRates[currency].ask) / 100 }</td>
+            <td>{ Math.round(value * 100 * (exchangeRates[currency].ask)) / 100 }</td>
+            <td>Real</td>
+            <td><button type="button" data-testid="delete-btn">deletar</button></td>
+          </tr>
+        ))}
+      </session>
+    );
+  }
+}
+
+TableExpenses.propTypes = { expenses: arrayOf().isRequired };
+
+const mapStateToProps = ({ wallet }) => ({ expenses: wallet.expenses });
+
+export default connect(mapStateToProps)(TableExpenses);
