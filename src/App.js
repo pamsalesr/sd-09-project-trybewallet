a37e4c9cdc5579c1445dde7113f50738ec8ea595
MatheuzaoCarvalho <matheusvscarvalho@gmail.com>
2021-04-06 01:45:07 -0300

    Hopefully task 01 - ok

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
 import React from 'react';
+import { Switch, Route } from 'react-router-dom';
 
-function App() {
-  return <div>Hello, TrybeWallet! Lets Go!</div>;
+import Login from './pages/Login';
+
+import './styles/global.css';
+
+class App extends React.Component {
+  render() {
+    return (
+      <Switch>
+        <Route exact path="/" component={ Login } />
+      </Switch>
+    );
+  }
 }
 
 export default App;
-
-// testing git flow
