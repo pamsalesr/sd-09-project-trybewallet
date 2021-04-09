c5b4eca679e54ffec2617c0c787ad3e247f98b82
fomes <contato.felipeluna@gmail.com>
2021-04-09 17:40:35 -0300

    rewrite wallet page

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -15,27 +15,27 @@ class Login extends React.Component {
       isValidPassword: '',
     };
 
-    // this.handleChange = this.handleChange.bind(this);
+    this.handleChange = this.handleChange.bind(this);
     this.onChangeEmail = this.onChangeEmail.bind(this);
     this.onChangePassword = this.onChangePassword.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
   }
 
-  // handleChange({ target }) {
-  //   const { name } = target;
-  //   this.setState({
-  //     [name]: target.value,
-  //   },
-  //   () => {
-  //     const { email, pass } = this.state;
-  //     const min = 5;
-  //     const regex = /^[a-z0-9.]+@[a-z0-9]+\.[a-z]+$/;
+  handleChange({ target }) {
+    const { name } = target;
+    this.setState({
+      [name]: target.value,
+    },
+    () => {
+      const { email, pass } = this.state;
+      const min = 5;
+      const regex = /^[a-z0-9.]+@[a-z0-9]+\.[a-z]+$/;
 
-  //     this.setState({
-  //       disable: pass.length > min && regex.test(email),
-  //     });
-  //   });
-  // }
+      this.setState({
+        disable: pass.length > min && regex.test(email),
+      });
+    });
+  }
 
   onChangeEmail(e) {
     const { value } = e.target;
--- src/pages/Wallet.js
+++ src/pages/Wallet.js
@@ -1,10 +1,8 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
-import get from '../services/api';
+// import get from '../services/api';
 import { walletThunk } from '../actions';
-import Table from '../components/Table';
-import Wallet2 from './Wallet2';
 
 class Wallet extends Component {
   constructor() {
@@ -30,85 +28,9 @@ class Wallet extends Component {
     this.handleApi();
   }
 
-  totalExp() {
-    const { expenses } = this.props;
-    let sum = 0;
-    expenses.forEach((expense) => {
-      const valueFloat = parseFloat(expense.value);
-      const float = parseFloat(expense.eRates[expense.currency].ask);
-      sum += valueFloat * float;
-    });
-    return sum.toFixed(2);
-  }
-
-  async handleApi() {
-    const result = await get();
-    const cKeys = Object.keys(result);
-    const curr = cKeys.filter((item) => item !== 'USDT');
-    this.setState({ currencies: curr });
-  }
-
-  handleInputChange(target) {
-    const { expenses } = this.state;
-    this.setState({
-      expenses: { ...expenses, [target.name]: target.value },
-    });
-  }
-
-  storeExp() {
-    const { saveExpenses } = this.props;
-    const { expenses } = this.state;
-    saveExpenses(expenses);
-    this.setState((prevState) => ({
-      expenses: { ...prevState.expenses, id: prevState.expenses.id + 1, value: 0 },
-    }));
-  }
-
   render() {
-    const sum = this.totalExp();
-    const { currencies, expenses: { value, description } } = this.state;
-    const cSelect = currencies.map((currencie) => (
-      <option key={ currencie } data-testid={ currencie } value={ currencie }>
-        {currencie}
-      </option>
-    ));
-
-    const { email } = this.props;
-    return (
-      <div>
-        <header className="header">
-          <h1>Trybe</h1>
-          <span>Email:</span>
-          <span data-testid="email-field">{ email }</span>
-          <span>Despesa Total: R$</span>
-          <span data-testid="total-field">
-            {sum}
-          </span>
-          <span data-testid="header-currency-field"> BRL </span>
-        </header>
-        <form>
-          <label htmlFor="value">
-            <span>Valor: </span>
-            <input
-              data-testid="value-input"
-              type="number"
-              name="value"
-              id="value"
-              value={ value }
-              onChange={ (e) => this.handleInputChange(e.target) }
-            />
-          </label>
-          <Wallet2 />
-          <button
-            type="button"
-            onClick={ () => this.storeExp() }
-          >
-            Adicionar despesa
-          </button>
-        </form>
-        <Table />
-      </div>
-    );
+    <>
+    </>
   }
 }
 
