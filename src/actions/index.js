5f1b365248cf837dc8b7bb661d3cb15a41b36c45
Caio Daniel Nunes Santos <caio.daniel.nunes.santos@gmail.com>
2021-04-12 19:48:00 -0300

    No MD5

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,10 +1,9 @@
 // Coloque aqui suas actions
-import md5 from 'crypto-js/md5';
 
 export const SAVE_LOGIN_INFO = 'SAVE_LOGIN_INFO';
 
 export const saveLoginInfo = (data) => ({
   type: SAVE_LOGIN_INFO,
   inputEmail: data.email,
-  inputPassword: md5(data.password).toString(),
+  inputPassword: data.password,
 });
