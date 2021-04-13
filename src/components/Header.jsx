a386d799955b16b84ddaa2e7160d4d5b681c1fab
Max <gmaxgomes@gmail.com>
2021-04-13 01:17:42 -0300

    lint fixes

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
 
 class Header extends React.Component {
   render() {
@@ -9,18 +10,25 @@ class Header extends React.Component {
         <section
           name="userData"
         >
-          <h3 data-testid="email-field">Bem vindo, { userEmail }</h3>
+          <h3 data-testid="email-field">
+            Bem vindo,
+            { userEmail }
+          </h3>
         </section>
         <section
           name="walletData"
         >
           <p data-testid="total-field">Gastos totais: 0</p>
-          <p data-testid="header-currency-field">Câmbio: 'BRL'</p>
+          <p data-testid="header-currency-field">Câmbio: BRL</p>
         </section>
       </>
     );
   }
 }
+
+Header.propTypes = {
+  userEmail: PropTypes.string.isRequired,
+};
 
 const mapStateToProps = (state) => ({
   userEmail: state.user.email,
