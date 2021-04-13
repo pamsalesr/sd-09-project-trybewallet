6fbe64b00d724e36e3dca5845f0c8e58cec9afc5
Caio Daniel Nunes Santos <caio.daniel.nunes.santos@gmail.com>
2021-04-13 12:32:37 -0300

    No auto go back to login

--- src/pages/Wallet.js
+++ src/pages/Wallet.js
@@ -1,37 +1,17 @@
 import React from 'react';
-import PropTypes from 'prop-types';
-import { connect } from 'react-redux';
-import { Redirect } from 'react-router-dom';
 
 import Header from '../components/Header';
 import AddSpending from '../components/AddSpending';
 
 class Wallet extends React.Component {
   render() {
-    const { email, passwordMD5 } = this.props;
-    return ((email && passwordMD5) ? (
+    return (
       <>
         <Header />
         <AddSpending />
       </>
-    ) : <Redirect to="/" />
     );
   }
 }
 
-const mapStateToProps = (state) => ({
-  email: state.user.email,
-  passwordMD5: state.user.passwordMD5,
-});
-
-Wallet.propTypes = {
-  email: PropTypes.string,
-  passwordMD5: PropTypes.string,
-};
-
-Wallet.defaultProps = {
-  email: null,
-  passwordMD5: null,
-};
-
-export default connect(mapStateToProps)(Wallet);
+export default Wallet;
