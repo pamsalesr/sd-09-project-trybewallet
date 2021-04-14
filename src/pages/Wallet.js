8fb6e55f66a927a4b6c643163c509f816c5d18eb
Uibirá <usbarreto@gmail.com>
2021-04-14 09:20:15 -0300

    pra nada

--- src/pages/Wallet.js
+++ src/pages/Wallet.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { string } from 'prop-types';
 import HeaderComponent from '../components/HeaderComponent';
 import TableComponent from '../components/TableComponent';
-import NewExpenseComponent from '../components/NewExpenseComponent';
+// import NewExpenseComponent from '../components/NewExpenseComponent';
 
 class Wallet extends React.Component {
   render() {
