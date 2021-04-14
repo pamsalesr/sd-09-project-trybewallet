3432ce43a31329dfb8b6ca8b215de738fb0febbd
Uibirá <usbarreto@gmail.com>
2021-04-14 09:17:41 -0300

    Projeto do inferno maldito

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import Wallet from './pages/Wallet';
 function App() {
   return (
     <div>
-      Hello, TrybeWallet! \\//
+      {/* Hello, TrybeWallet! \\// */}
       <BrowserRouter>
         <Route exact path="/" component={ Login } />
         <Route path="/carteira" component={ Wallet } />
--- src/pages/Wallet.js
+++ src/pages/Wallet.js
@@ -2,12 +2,14 @@ import React from 'react';
 import { string } from 'prop-types';
 import HeaderComponent from '../components/HeaderComponent';
 import TableComponent from '../components/TableComponent';
+import NewExpenseComponent from '../components/NewExpenseComponent';
 
 class Wallet extends React.Component {
   render() {
     return (
       <div>
         <HeaderComponent />
+        {/* <NewExpenseComponent /> */}
         <TableComponent />
       </div>);
   }
