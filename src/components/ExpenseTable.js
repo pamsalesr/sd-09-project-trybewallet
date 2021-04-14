18e8a9bf658e8874f1a09b7dd6719ed6be71a317
Nícolas Tozato <ntozato9@gmail.com>
2021-04-14 09:14:53 -0300

    proptypes

--- src/components/ExpenseTable.js
+++ src/components/ExpenseTable.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 
 class ExpenseTable extends React.Component {
@@ -53,6 +54,10 @@ class ExpenseTable extends React.Component {
     );
   }
 }
+
+ExpenseTable.propTypes = {
+  expenses: PropTypes.arrayOf(PropTypes.object).isRequired,
+};
 
 const mapStateToProps = (state) => ({
   expenses: state.wallet.expenses,
