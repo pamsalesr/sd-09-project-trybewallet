8269ed0c2fb439679968ea306e4bf68d8cbc9a9a
BBrolesi <brunohsbrolesi@gmail.com>
2021-04-14 23:00:06 -0300

    feature: fetch currency api, and remove USDT from json

---
+++ src/services/api.js
@@ -0,0 +1,15 @@
+async function fetchCurrency() {
+  const endpoint = 'https://economia.awesomeapi.com.br/json/all';
+  try {
+    const response = await fetch(endpoint);
+    const currencies = await response.json();
+    if ('USDT' in currencies) {
+      delete currencies.USDT;
+    }
+    return currencies;
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+export default fetchCurrency;
