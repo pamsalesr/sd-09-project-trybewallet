f6a24cc6f374fc5b172452e30290845961426622
Leticia Lima <letylima96@gmail.com>
2021-04-14 22:26:48 -0300

    requisito 4

--- src/pages/Wallet.js
+++ src/pages/Wallet.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { string } from 'prop-types';
 import { connect } from 'react-redux';
 import Header from '../components/Header';
+import WholeFormWithBttn from '../components/WholeFormWithBttn';
 
 class Wallet extends React.Component {
   render() {
@@ -11,6 +12,7 @@ class Wallet extends React.Component {
         <Header
           email={ email }
         />
+        <WholeFormWithBttn />
       </div>
     );
   }
