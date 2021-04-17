f3df2ee3b06be560f3cda1bddb8a1365bf063c4a
Micaela Alves <micaela.alves93@gmail.com>
2021-04-17 04:32:22 -0300

    config reduce -  reducer wallet

--- src/reducers/wallet.js
+++ src/reducers/wallet.js
@@ -1,1 +1,22 @@
 // Esse reducer será responsável por tratar o todas as informações relacionadas as despesas
+import { WALLET } from '../actions';
+
+const INITIAL_STATE = {
+  wallet: {
+    currencies: [],
+    expenses: [],
+  },
+};
+
+const wallet = (state = INITIAL_STATE, action) => {
+  switch (action.type) {
+  case WALLET:
+    return {
+      state: action.state,
+    };
+  default:
+    return state;
+  }
+};
+
+export default wallet;
