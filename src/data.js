103bb37182653e71cbcb2f9293b16f5bf0667962
Micaela Alves <micaela.alves93@gmail.com>
2021-04-17 13:29:59 -0300

    arquivo com dados

---
+++ src/data.js
@@ -0,0 +1,15 @@
+const payMethods = [
+  'Dinheiro',
+  'Cartão de crédito',
+  'Cartão de débito',
+];
+
+const tagExpense = [
+  'Alimentação',
+  'Lazer',
+  'Trabalho',
+  'Transporte',
+  'Saúde',
+];
+
+export { payMethods, tagExpense };
