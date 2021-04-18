d623176f2db449a3f8affa5eca3ee382afd25870
Celso Oliva Aparecido <celso.oliva.aparecido@gmail.com>
2021-04-18 01:33:46 -0300

    feat(component/Header): adding Header and connect redux

---
+++ src/components/Header.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+
+class Header extends React.Component {
+  render() {
+    const { email, total, currency } = this.props;
+    return (
+      <header>
+        <span data-testid="email-field">
+          Email:&nbsp;
+          {email}
+        </span>
+        <span data-testid="total-field">
+          Total:&nbsp;
+          {total}
+        </span>
+        <span data-testid="header-currency-field">
+          Moeda:&nbsp;
+          {currency}
+        </span>
+      </header>
+    );
+  }
+}
+
+const mapStateToProps = (state) => ({
+  email: state.user.email,
+});
+
+Header.propTypes = {
+  email: PropTypes.string.isRequired,
+  total: PropTypes.number,
+  currency: PropTypes.string,
+};
+
+Header.defaultProps = {
+  total: 0,
+  currency: 'BRL',
+};
+
+export default connect(mapStateToProps)(Header);
--- src/reducers/wallet.js
+++ src/reducers/wallet.js
@@ -1,1 +1,20 @@
-// Esse reducer será responsável por tratar o todas as informações relacionadas as despesas
+import { ADD_EXPENSE } from '../actions/types';
+
+const INITIAL_STATE = {
+  currencies: [],
+  expenses: [],
+};
+
+const walletReducer = (state = INITIAL_STATE, { type, payload }) => {
+  switch (type) {
+  case ADD_EXPENSE:
+    return {
+      ...state,
+      ...payload,
+    };
+  default:
+    return state;
+  }
+};
+
+export default walletReducer;
