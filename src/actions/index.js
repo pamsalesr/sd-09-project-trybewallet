edd493001259b9ba8180952b768245f71b0a0071
Bruno <brunocmoraes97@gmail.com>
2021-04-19 17:21:14 -0300

    Requisito 1

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,1 +1,6 @@
-// Coloque aqui suas actions
+export const CREATE_EMAIL = 'CORRECT_EMAIL';
+
+export const userEmail = (email) => ({
+  type: CREATE_EMAIL,
+  email,
+});
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,9 +1,95 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
+import { connect } from 'react-redux';
+import { userEmail } from '../actions';
 
 class Login extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      email: '',
+      disabled: true,
+      keyEmail: false,
+      keyPassowrd: false,
+    };
+    this.handleInputEmail = this.handleInputEmail.bind(this);
+    this.handleInputPassword = this.handleInputPassword.bind(this);
+    this.disabledBtn = this.disabledBtn.bind(this);
+  }
+
+  handleInputEmail({ target }) {
+    const validateEmail = /^[\S.]+@[a-z]+\.\w{2,3}$/g.test(target.value);
+    this.setState({
+      email: target.value,
+      keyEmail: validateEmail,
+    });
+  }
+
+  handleInputPassword({ target }) {
+    const six = 5;
+    if (target.value.length >= six) {
+      this.setState({
+        keyPassowrd: true,
+      });
+    } else {
+      this.setState({
+        keyPassowrd: false,
+      });
+    }
+  }
+
+  disabledBtn() {
+    const { dispatchEmail } = this.props;
+    const { keyEmail, keyPassowrd, email } = this.state;
+    dispatchEmail(email);
+    if (keyEmail && keyPassowrd) {
+      this.setState({
+        disabled: false,
+      });
+    } else {
+      this.setState({
+        disabled: true,
+      });
+    }
+  }
+
   render() {
-    return <div>Login</div>;
+    const { disabled } = this.state;
+    return (
+      <div>
+        <input
+          type="text"
+          data-testid="email-input"
+          placeholder="Email"
+          onChange={ (e) => {
+            this.handleInputEmail(e);
+            this.disabledBtn();
+          } }
+        />
+        <input
+          type="password"
+          data-testid="password-input"
+          placeholder="Senha"
+          onChange={ (e) => {
+            this.handleInputPassword(e);
+            this.disabledBtn();
+          } }
+        />
+        <Link to="/carteira">
+          <button type="button" disabled={ disabled }>Entrar</button>
+        </Link>
+      </div>
+    );
   }
 }
 
-export default Login;
+const mapDispatchToProps = (dispatch) => ({
+  dispatchEmail: (email) => (dispatch(userEmail(email))),
+});
+
+Login.propTypes = {
+  dispatchEmail: PropTypes.func.isRequired,
+};
+
+export default connect(null, mapDispatchToProps)(Login);
