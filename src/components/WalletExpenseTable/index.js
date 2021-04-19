1304b30ea907f8ca80aafb23971b52150f09a873
Rafael Gerônimo <rafaelgeronimo@outlook.com>
2021-04-19 14:31:23 -0300

    adiciona o arquivo css, que havia esquecido

--- src/components/WalletExpenseTable/index.js
+++ src/components/WalletExpenseTable/index.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
+import './styles.css';
 
 class WalletExpenseTable extends Component {
   renderExpenses(expenses) {
